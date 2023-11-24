12fcd948b73c5089481668e5768d729ead625cff
Dumssss <clementdumas11@gmail.com>
2023-11-24 15:27:35 +0100

    repare some bug Nous sommes l'équipe 4, composée de Fils, Clement, Dorian et Aedan.

--- hook/hook.sh
+++ hook/hook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 commit_msg=$(cat $1)
 commit_length=${#commit_msg}
-EXPECTED_MESSAGE=", nous sommes l'équipe 4, composée de Fils, Clement, Dorian et Aedan."
+EXPECTED_MESSAGE="Nous sommes l'équipe 4, composée de Fils, Clement, Dorian et Aedan."
 commit_msg="$commit_msg $EXPECTED_MESSAGE"
 echo "$commit_msg" > "$1"
